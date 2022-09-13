ea9e7f9b2eb64ab30830ef8bc7a46b4e0f37de00
Kogoon <gojs712@gmail.com>
2022-09-13 10:59:24 +0900

    Modified prod config

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 #BASE_DIR = os.path.dirname(__file__)
 
-DB_USER_NAME='admin'
-DB_USER_PASSWD='1q2w3e4r!'
-DB_HOST='database-1.cdfnd5ogvaqo.ap-northeast-2.rds.amazonaws.com'
-DB_NAME='flaskapp'
+DB_USER_NAME='brokurly'
+DB_USER_PASSWD='Kosa0401!'
+DB_HOST='brokurlydatabase.cdfnd5ogvaqo.ap-northeast-2.rds.amazonaws.com'
+DB_NAME='brokurlyapp'
 
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://testdb.cdfnd5ogvaqo.ap-northeast-2.rds.amazonaws.com'
 SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@{}/{}?charset=utf8'.format(DB_USER_NAME, DB_USER_PASSWD, DB_HOST, DB_NAME)
