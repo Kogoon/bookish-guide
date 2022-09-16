d76c4df7b723f8a451337dd6897f714a6378b60d
Kogoon <gojs712@gmail.com>
2022-09-16 16:55:52 +0900

    Modified DB

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 #BASE_DIR = os.path.dirname(__file__)
 
-#DB_USER_NAME='brokurly'
-DB_USER_NAME='admin'
-#DB_USER_PASSWD='Kosa0401!'
-DB_USER_PASSWD='1q2w3e4r!'
-#DB_HOST='brokurlydatabase.cdfnd5ogvaqo.ap-northeast-2.rds.amazonaws.com'
-DB_HOST='testdb.cdfnd5ogvaqo.ap-northeast-2.rds.amazonaws.com'
-#DB_NAME='brokurlyapp'
-DB_NAME='flaskapp'
+DB_USER_NAME='brokurly'
+#DB_USER_NAME='admin'
+DB_USER_PASSWD='Kosa0401!'
+#DB_USER_PASSWD='1q2w3e4r!'
+DB_HOST='brokurlydatabase.cdfnd5ogvaqo.ap-northeast-2.rds.amazonaws.com'
+#DB_HOST='testdb.cdfnd5ogvaqo.ap-northeast-2.rds.amazonaws.com'
+DB_NAME='brokurlyapp'
+#DB_NAME='flaskapp'
 
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://testdb.cdfnd5ogvaqo.ap-northeast-2.rds.amazonaws.com'
 SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@{}/{}?charset=utf8'.format(DB_USER_NAME, DB_USER_PASSWD, DB_HOST, DB_NAME)
